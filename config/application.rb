18e6f16c46035135390e25cbbb533b05e947dcbb
Emily Fiennes <etw.fiennes@gmail.com>
2016-11-21 19:00:16 +0100

    Fix tokens and config for heroku

--- config/application.rb
+++ config/application.rb
@@ -16,15 +16,16 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# Auto-load the bot and its subdirectories
-config.paths.add File.join('app', 'bot'), glob: File.join('**', '*.rb')
-config.autoload_paths += Dir[Rails.root.join('app', 'bot', '*')]
+
 
 module Spotter
   class Application < Rails::Application
     config.generators do |generate|
       generate.assets false
     end
+    # Auto-load the bot and its subdirectories
+    config.paths.add File.join('app', 'bot'), glob: File.join('**', '*.rb')
+    config.autoload_paths += Dir[Rails.root.join('app', 'bot', '*')]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
