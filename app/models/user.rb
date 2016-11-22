3f74be46de64a5b388d4f1e86d74cd416807b8cd
Audrey Labuxiere <audreylabuxiere@gmail.com>
2016-11-22 13:16:46 +0100

    update seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   #has_many :events
   has_many :participations
-  #validates :first_name, presence: true
-  #validates :last_name, presence: true
-  #validates :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: true
 end
