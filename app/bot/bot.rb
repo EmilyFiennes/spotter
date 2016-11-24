91280abfe2d6cb2c86609497a11852bb71f29bb2
Emily Fiennes <etw.fiennes@gmail.com>
2016-11-24 19:12:32 +0100

    start create event

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -29,19 +29,42 @@ include Facebook::Messenger
 #   access_token: ENV["ACCESS_TOKEN"]
 # )
 
+@start_hour_asked = false
+
 
 Bot.on :message do |message|
+  BOT_SESSION ||= {}
   timestamp = message.messaging['timestamp'].to_i / 1000
   date = DateTime.strptime(timestamp.to_s, '%s').strftime('%d-%m-%Y %H:%M')
   puts "Received '#{message.inspect}' from #{message.sender} at #{date}"
 
-  case message.text
-  when /hello/i
+  if /hello/i.match(message.text)
     @bot_threads_controller.welcome(message)
     @bot_threads_controller.initial_choice(message)
-  when "Start again"
+  elsif message.text == "Start again"
     @bot_threads_controller.initial_choice(message)
+  elsif /\A(?<day>\d+)\/(?<month>\d+)\/(?<year>\d+)\z/i.match(message.text)
+    BOT_SESSION[:day] = $LAST_MATCH_INFO['day'].to_i
+    BOT_SESSION[:month] = $LAST_MATCH_INFO['month'].to_i
+    BOT_SESSION[:year] = $LAST_MATCH_INFO['year'].to_i
+    # date = Date.new(@year, @month, @day)
+    @bot_events_controller.gets_start_time(message)
+    #@bot_events_controller.new_event(message, date: date)
+  elsif @start_hour_asked == false && /(?<start_hour>\d+)\:(?<minute>\d+)/i.match(message.text)
+    BOT_SESSION[:start_hour] = $LAST_MATCH_INFO['start_hour'].to_i
+    BOT_SESSION[:start_minute] = $LAST_MATCH_INFO['minute'].to_i
+    # start_time = Time.new(@year, @month, @day, @start_hour, minute)
+    @start_hour_asked = true
+    @bot_events_controller.gets_end_time(message)
+  elsif /(?<end_hour>\d+)\:(?<minute>\d+)/i.match(message.text)
+    BOT_SESSION[:end_hour] = $LAST_MATCH_INFO['end_hour'].to_i
+    BOT_SESSION[:end_minute] = $LAST_MATCH_INFO['minute'].to_i
+    #end_time = Time.new(@year, @month, @day, @start_hour, minute)
+    @start_hour_asked = false
+    #do something to stock time and date
+    @bot_events_controller.gets_activity(message)
   else
+
     message.reply(
       text: "Say 'hello' to start"
     )
@@ -53,14 +76,16 @@ Bot.on :postback do |postback|
   case postback.payload
   when 'FIND'
     @bot_threads_controller.gets_day(postback)
-  # when 'CREATE'
-  #   @bot_events_controller.new(postback)
+  when 'CREATE'
+    @bot_events_controller.gets_day(postback)
   when /date/i
     @bot_events_controller.set_date(postback)
     @bot_threads_controller.gets_activity(postback)
   when /activity/i
     @bot_events_controller.set_activity(postback)
     @bot_events_controller.index(postback)
+  when /choice/i
+    @bot_events_controller.set_create_date(postback)
   end
 end
 
--- app/bot/views/bot_events_view.rb
+++ app/bot/views/bot_events_view.rb
@@ -45,4 +45,80 @@ class BotEventsView
       )
     end
   end
+
+  def create_now_or_later(postback)
+    postback.reply(
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'button',
+          text: 'Do you want to create an event for today or later?',
+          buttons: [
+            { type: 'postback', title: 'Today', payload: 'choice_today' },
+            { type: 'postback', title: 'Later', payload: 'choice_later' }
+          ]
+        }
+      }
+    )
+  end
+
+  def enter_date(postback)
+    postback.reply(
+      text: "Please enter the date of your activity e.g. 24/11/2016"
+      )
+  end
+
+  def enter_start_time(postback)
+    postback.reply(
+      text: "Please enter the time your activity will start at eg. 17h30"
+      )
+  end
+
+  def enter_end_time(postback)
+    postback.reply(
+      text: "Please enter the time your activity will end at e.g. 19h30")
+  end
+
+  def full_activity_list(postback)
+    postback.reply(
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+            title: "Running",
+            image_url: "https://conceptdraw.com/a2327c3/p13/preview/640/pict--running-man-people-pictograms---vector-stencils-library.png--diagram-flowchart-example.png",
+            buttons: [
+              { type: 'postback', title: "Select", payload: 'activity_running' }
+            ]},
+            {
+            title: "Swimming",
+            image_url: "https://conceptdraw.com/a2327c3/p13/preview/640/pict--running-man-people-pictograms---vector-stencils-library.png--diagram-flowchart-example.png",
+            buttons: [
+              { type: 'postback', title: "Select", payload: 'activity_swimming' }
+            ]},
+            {
+            title: "Soccer",
+            image_url: "https://conceptdraw.com/a2327c3/p13/preview/640/pict--running-man-people-pictograms---vector-stencils-library.png--diagram-flowchart-example.png",
+            buttons: [
+              { type: 'postback', title: "Select", payload: 'activity_soccer' }
+            ]},
+            {
+            title: "Tennis",
+            image_url: "https://conceptdraw.com/a2327c3/p13/preview/640/pict--running-man-people-pictograms---vector-stencils-library.png--diagram-flowchart-example.png",
+            buttons: [
+              { type: 'postback', title: "Select", payload: 'activity_tennis' }
+            ]},
+            {
+            title: "Surprise me",
+            image_url: "https://conceptdraw.com/a2327c3/p13/preview/640/pict--running-man-people-pictograms---vector-stencils-library.png--diagram-flowchart-example.png",
+            buttons: [
+              { type: 'postback', title: "Select", payload: 'activity_suprise' }
+            ]}
+          ]
+        }
+      }
+    )
+  end
 end
