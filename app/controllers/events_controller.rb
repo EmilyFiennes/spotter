69dc750c707fd81ab171562f5b2a0e40998cd15d
sebastien goyer <seb.goyer@gmail.com>
2016-11-24 19:26:30 +0100

    homepageavatar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+  skip_before_action :authenticate_user!
   def show
     @event = Event.find(params[:id])
     @start_at = Date.parse("#{@event.start_at}").strftime("%a, %d %b %Y")
