06eb3165559cdbea43582248b8486f07c5f7fe08
Audrey Labuxiere <audreylabuxiere@gmail.com>
2016-11-28 16:10:50 +0100

    other updates on event view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,8 +2,10 @@ class EventsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
 
   def show
+    @user = current_user
     @event = Event.find(params[:id])
-    @start_at = Date.parse("#{@event.start_at}").strftime("%d %b %Y at %H:%m")
-    @end_at = Date.parse("#{@event.end_at}").strftime("%d %b %Y at %H:%m")
+    @date = Date.parse("#{@event.start_at}").strftime("%d %b %Y")
+    @start_time = Date.parse("#{@event.start_at}").strftime("%H:%m")
+    @end_time = Date.parse("#{@event.end_at}").strftime("%H:%m")
   end
 end
