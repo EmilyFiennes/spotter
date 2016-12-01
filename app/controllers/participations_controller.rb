659c8f72333f8e0ee183aac707f73e7b470d305b
Emily Fiennes <etw.fiennes@gmail.com>
2016-12-01 19:47:24 +0100

    debug delete participation x 2

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -4,7 +4,7 @@ class ParticipationsController < ApplicationController
   def destroy
     @participation = Participation.find(params[:id])
     if @participation.destroy
-      flash[:success] = "You are no longer participating in the #{@participation.event.activity.name} session with #{@participation.user.name} on #{@participation.event.start_at.strftime('%d-%m-%Y')}"
+      flash[:success] = "You are no longer participating in the #{@participation.event.activity.name} session with #{@participation.user.first_name} on #{@participation.event.start_at.strftime('%d-%m-%Y')}"
     end
     redirect_to user_path(current_user)
   end
