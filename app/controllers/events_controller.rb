741c1bc4629709278264db5537f4ab9ded74b7e2
Emily Fiennes <etw.fiennes@gmail.com>
2016-12-01 12:41:29 +0100

    destroy routes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :show ]
+  skip_before_action :authenticate_user!, only: [ :show, :delete ]
 
   def show
     @user = current_user
@@ -7,5 +7,14 @@ class EventsController < ApplicationController
     @date = @event.start_at.strftime("%d %b %Y")
     @start_time = @event.start_at.strftime("%H:%m")
     @end_time = @event.end_at.strftime("%H:%m")
+  end
+
+  def destroy
+    user = current_user
+    @event = Event.find(params[:id])
+    if @event.destroy
+      flash[:alert] = "Your event #{@event.activity.name} on #{@event.start_at} has been successfully deleted."
+    end
+    redirect_to user_path(user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   root to: 'pages#home'
-  resources :events, only: [:show]
+  resources :events, only: [:show, :destroy]
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
