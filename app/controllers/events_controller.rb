491197d3794797031ae137fa81b936da3db3c6a2
Emily Fiennes <etw.fiennes@gmail.com>
2016-12-01 17:59:44 +0100

    fix errors on dates and redeploy

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,8 +5,8 @@ class EventsController < ApplicationController
     @user = current_user
     @event = Event.find(params[:id])
     @date = @event.start_at.strftime("%d %b %Y")
-    @start_time = @event.start_at.strftime("%H:%m")
-    @end_time = @event.end_at.strftime("%H:%m")
+    @start_time = @event.start_at.strftime("%H:%M")
+    @end_time = @event.end_at.strftime("%H:%M")
   end
 
   def destroy
