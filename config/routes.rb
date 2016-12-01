799530c881c507b2ff7849637ae498c43cb1c832
Emily Fiennes <etw.fiennes@gmail.com>
2016-12-01 14:11:32 +0100

    udpate if / cancel on participation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :participations, only: [:destroy], shallow: :true
+  end
   root to: 'pages#home'
   resources :events, only: [:show, :destroy]
   devise_for :users,
